ba9cdff9730d4ecb9495d8b95983822e07ca12af
Cameron Elliott <868689+cameronelliott@users.noreply.github.com>
2023-10-03 15:16:55 -0700

    add initial useWhipHook()

--- src/common.ts
+++ src/common.ts
@@ -1,29 +1,29 @@
 import React from 'react'
 import { useState, useEffect, useRef } from 'react'
 import { WHEPClient } from '@cameronelliott/whip-whep/whep.js'
+import { WHIPClient } from '@cameronelliott/whip-whep/whip.js'
 
 //XXX bad
-//export {React} 
-
-
+//export {React}
 
 // to enable debug output, in the chrome console, type: localStorage.setItem('whep-debug', '1')
 // to disable debug output, in the chrome console, type: localStorage.removeItem('whep-debug')
-export const debug = (...args: any[]) => localStorage.getItem('whep-debug') && console.log(...args)
+export const debug = (...args: any[]) =>
+  localStorage.getItem('whep-debug') && console.log(...args)
 
 export type PcOrNull = RTCPeerConnection | null
 export type PcRef = React.MutableRefObject<PcOrNull>
 export type MediaStreamOrNull = MediaStream | null
 
-export const pcConf: RTCConfiguration = { iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] }
+export const pcConf: RTCConfiguration = {
+  iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
+}
 
 export async function mysleep() {
   debug('-- mysleep')
   const seconds = 3
   return new Promise((resolve) => setTimeout(resolve, seconds * 1000))
 }
-
-
 
 export function destroyPCRef(ref: PcRef) {
   debug('-- destroyPCRef')
@@ -49,7 +49,6 @@ export function destroyPCRef(ref: PcRef) {
   // I did #1 for testing
   // I also testing breaking this function to confirm my test was valid
 }
-
 
 export function useWhepHook(
   url: string,
@@ -104,7 +103,7 @@ export function useWhepHook(
       }
     }
 
-    if (true) {
+    if (pcref.current && pcref.current.iceConnectionState !== 'connected') {
       whepRef
         .current!.view(pcref.current, url, token)
         .then(() => {
@@ -131,4 +130,71 @@ export function useWhepHook(
   return [pcref, mediaStream]
 }
 
+export function useWhipHook(
+  mediaStream: MediaStream,
+  url: string,
+  token?: string
+): PcRef {
+  const [ntries, setNtries] = useState<number>(0)
+  //cannot call useRef in useEffect
+  const pcref = useRef<PcOrNull>(null)
 
+  //cannot call useRef in useEffect
+  const whipRef = useRef<WHIPClient | null>(null)
+
+  const forceRender = () => setNtries(ntries + 1)
+
+  useEffect(() => {
+    debug('-- useWhipHook() useEffect entry')
+
+    if (pcref.current === null) {
+      pcref.current = new RTCPeerConnection(pcConf)
+
+      for (const track of mediaStream.getTracks()) {
+        pcref.current.addTransceiver(track, { direction: 'sendonly' })
+      }
+    }
+    if (whipRef.current === null) {
+      whipRef.current = new WHIPClient()
+    }
+
+    pcref.current!.oniceconnectionstatechange = (ev) => {
+      const st = (ev.target as RTCPeerConnection).iceConnectionState
+      debug('-- ice state change', st)
+
+      if (st === 'disconnected' || st === 'failed' || st === 'closed') {
+        debug('-- ice state closed/etc, forcing render')
+        //XXX
+        // I am concerned these two state touches might cause two re - renders
+        // but I hope not
+        forceRender()
+        //setMediaStream(null)
+      }
+    }
+
+    if (pcref.current && pcref.current.iceConnectionState !== 'connected') {
+      whipRef
+        .current!.publish(pcref.current, url, token)
+        .then(() => {
+          debug('-- whip.publish done OK')
+        })
+        .catch((err: Error) => {
+          debug('-- whip.publish done ERR / sleeping', err)
+          mysleep().then(() => {
+            debug('-- whip.publish done ERR, forcing render')
+            forceRender()
+          })
+        })
+    }
+
+    return () => {
+      debug('-- useWhipHook() useEffect cleanup')
+      destroyPCRef(pcref)
+      whipRef.current!.stop().catch(() => {
+        debug('-- whip.stop() done ERR')
+      })
+    }
+  }, [ntries]) // no-array: every render, []: once, [ntries]: when ntries changes
+
+  return pcref
+}
