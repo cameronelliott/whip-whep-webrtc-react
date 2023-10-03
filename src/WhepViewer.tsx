c8dfbe23a005e41d166e2c62b5125434ce15976e
Cameron Elliott <868689+cameronelliott@users.noreply.github.com>
2023-10-03 13:55:53 -0700

    no semis

--- src/WhepViewer.tsx
+++ src/WhepViewer.tsx
@@ -1,29 +1,29 @@
-import { useEffect, useRef } from 'react';
-import { debug, useWhepHook } from './common.js';
+import { useEffect, useRef } from 'react'
+import { debug, useWhepHook } from './common.js'
 
 export type WhepViewerProps = {
-  url: string;
-  token?: string;
-};
+  url: string
+  token?: string
+}
 
 export function WhepViewer(props: WhepViewerProps) {
-  debug('Entered WhepViewer-nonnative');
+  debug('Entered WhepViewer-nonnative')
 
-  const vidref = useRef<HTMLVideoElement>(null);
+  const vidref = useRef<HTMLVideoElement>(null)
 
-  const [pcref, mediaStream] = useWhepHook(props.url, props.token);
+  const [pcref, mediaStream] = useWhepHook(props.url, props.token)
 
   useEffect(() => {
     if (vidref.current) {
-      vidref.current.srcObject = mediaStream;
+      vidref.current.srcObject = mediaStream
     }
-  }, [mediaStream, vidref]);
+  }, [mediaStream, vidref])
 
-  debug('ParentComponent returning JSX');
+  debug('ParentComponent returning JSX')
 
   if (mediaStream === null) {
-    return <video autoPlay={true} muted controls />;
+    return <video autoPlay={true} muted controls />
   } else {
-    return <video autoPlay={true} muted controls ref={vidref} />;
+    return <video autoPlay={true} muted controls ref={vidref} />
   }
 }
