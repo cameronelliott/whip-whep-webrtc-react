ea54e6aa00a002bd891e52a8deb4df743a48bffd
Cameron Elliott <868689+cameronelliott@users.noreply.github.com>
2023-10-03 15:16:25 -0700

    Initial whip sender

---
+++ src/WhipSender.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useRef } from 'react'
+import { debug, useWhipHook } from './common.js'
+
+export type WhipSenderProps = {
+  mediaStream: MediaStream
+  url: string
+  token?: string
+}
+
+export function WhipSender(props: WhipSenderProps) {
+  debug('Entered WhepViewer')
+
+  const pcref = useWhipHook(props.mediaStream, props.url, props.token)
+
+  // useEffect(() => {
+  //   if (vidref.current) {
+  //     vidref.current.srcObject = mediaStream
+  //   }
+  // }, [mediaStream, vidref])
+
+  debug('WhepViewer returning JSX')
+
+  // if (mediaStream === null) {
+  //   return <video autoPlay={true} muted controls />
+  // } else {
+  //   return <video autoPlay={true} muted controls ref={vidref} />
+  // }
+
+  return
+}
