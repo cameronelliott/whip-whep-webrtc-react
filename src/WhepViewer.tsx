4ace0eb0ab69012f60c33812a1f684ec03897e73
Cameron Elliott <868689+cameronelliott@users.noreply.github.com>
2023-10-06 13:12:05 -0700

    move useWhepUseEffect to common

--- src/WhepViewer.tsx
+++ src/WhepViewer.tsx
@@ -5,6 +5,7 @@ import {
   destroyPCRef,
   mysleep,
   pcConf,
+  useWhepUseEffect,
 } from './common.js'
 import { WHEPClient } from '@cameronelliott/whip-whep/whep.js'
 
@@ -39,91 +40,6 @@ export type WhepConnectProps = {
   token: string
   setMediaStream: (ms: MediaStream) => void
   forceRender: () => void
-}
-
-export function useWhepUseEffect(
-  url: string,
-  token?: string
-): [MediaStream, boolean] {
-  //
-  debug('-- useWhepUseEffect() entry')
-
-  const [counter, setCounter] = useState(0) // this is used to restart a connection
-  const [conn, setConnected] = useState(false) // this is used change css styles
-  const forceRender = () => setCounter(counter + 1) // convienence function
-
-  // this only gets created once, and is never destroyed
-  // tracks get added and removed from it inside ontrack()
-  const stream = useRef<MediaStreamOrNull>(null)
-  if (stream.current === null) {
-    stream.current = new MediaStream()
-  }
-
-  useEffect(() => {
-    debug('-- useWhepHook() useEffect entry')
-
-    const pc = new RTCPeerConnection(pcConf)
-
-    const whep = new WHEPClient()
-
-    pc.ontrack = (ev) => {
-      debug('-- ontrack entry kind: ', ev.track.kind)
-      if (ev.streams[0]) {
-        // delete the existing tracks
-        stream
-          .current!.getTracks()
-          .forEach((t) => stream.current!.removeTrack(t))
-
-        debug('-- ontrack got stream')
-        for (const tr of ev.streams[0].getTracks()) {
-          debug('-- ontrack receiver kind: ', tr.kind)
-          stream.current!.addTrack(tr)
-        }
-      } else {
-        console.warn('-- ontrack empty stream')
-      }
-    }
-    pc.addTransceiver('video', { direction: 'recvonly' })
-    pc.addTransceiver('audio', { direction: 'recvonly' })
-    pc.oniceconnectionstatechange = (ev) => {
-      const st = (ev.target as RTCPeerConnection).iceConnectionState
-      debug('-- ice state change', st)
-
-      if (st === 'disconnected' || st === 'failed' || st === 'closed') {
-        forceRender() // two state updates, two renders? :()
-        setConnected(false)
-      }
-
-      if (st === 'connected') {
-        setConnected(true)
-      }
-    }
-
-    whep
-      .view(pc, url, token)
-      .then(() => {
-        debug('-- whep.view() done OK')
-      })
-      .catch((err: Error) => {
-        debug('-- whep.view() done ERR / sleeping', err)
-        mysleep().then(() => {
-          debug('-- whep.view() done ERR, forcing render')
-          forceRender() // two state updates, two renders? :()
-          setConnected(false)
-        })
-      })
-
-    // return the cleanup function to be called on component unmount
-    return () => {
-      debug('-- useWhepReceiverHook() useEffect cleanup')
-      destroyPCRef(pc)
-      whep.stop().catch(() => {
-        debug('-- whep.stop() done ERR')
-      })
-    }
-  }, [counter]) // no-array: every render, []: just-a-single-time, [xxx]: when xxx changes
-
-  return [stream.current!, conn]
 }
 
 export function WhepViewer(props: WhepViewerProps) {
